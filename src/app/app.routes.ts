0ba9f18b492f7941130799b48eba11b8ef08f18e
efraim <efraimg@n-compass.biz>
2024-11-11 15:53:51 +0800

    second commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { UploaderComponent } from './components/uploader/uploader.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'dashboard', component: UploaderComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+];
