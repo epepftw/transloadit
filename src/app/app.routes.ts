ea6bbf9c85dd6c4321037b64e3f9b835113957ea
efraim <efraimg@n-compass.biz>
2024-11-11 17:09:45 +0800

    Revert "second commit"
    This reverts commit 0ba9f18b492f7941130799b48eba11b8ef08f18e.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,3 @@
 import { Routes } from '@angular/router';
-import { UploaderComponent } from './components/uploader/uploader.component';
 
-export const routes: Routes = [
-  { path: 'dashboard', component: UploaderComponent },
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-];
+export const routes: Routes = [];
